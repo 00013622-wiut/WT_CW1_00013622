5a2702d888a8f6bbccad23b796f8f68b9a6e5a46
00013622 <efirdavs03@gmail.com>
2022-11-23 11:53:20 -0800

    about page is set up for dynamic use

--- js/main.js
+++ js/main.js
@@ -2,3 +2,31 @@ import { $ } from "./helper.js"
 $("#myButton").addEventListener("click", function () {
     console.log("Button clicked");
 })
+
+// $("#card-body").appendChild(`
+// <div class="card">
+// <h1>About title</h1>
+// <p>
+//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat,
+//     tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.
+// </p>
+// </div>
+// `)
+
+// append to the card-body 
+
+
+for (let i = 0; i < 4; i++) {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+<h1>About title</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat,
+    tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.
+</p>
+`;
+    $("#card-body").appendChild(card);
+}
+
+
