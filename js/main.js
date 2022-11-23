33bc57683ab93b92b77428d8055ea1ba1e18d73c
00013622 <efirdavs03@gmail.com>
2022-11-23 12:00:54 -0800

    added dynamic functions

--- js/main.js
+++ js/main.js
@@ -15,12 +15,27 @@ $("#myButton").addEventListener("click", function () {
 
 // append to the card-body 
 
+if (window.location.pathname === "/about.html" || window.location.pathname === "/about") {
+    $(".navbar").className = "navbar-dark"
+}
+if (window.screen.width > 768) {
+    $(".navbar-menu-list").classList.remove("navbar-active")
+    $(".navbar-menu-list").classList.add("list")
+}
+
+$("#toggleMenu").addEventListener("click", function () {
+    // $(".navbar-menu-list").classList.toggle("list")
+    $(".navbar-menu-list").classList.toggle("navbar-active")
+})
+
+
+
 
 for (let i = 0; i < 4; i++) {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-<h1>About title</h1>
+<h1>About Card</h1>
 <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat,
     tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.
