9f37f2ce8a85752c04d2bdabf56e92e16b1165e7
00013622 <efirdavs03@gmail.com>
2022-11-24 07:09:17 -0800

    stuff typo corrected

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import { $ } from "./helper.js"
-
+import { $ } from "./helper.js";
 
 const navbarContainer = document.createElement("div");
 navbarContainer.className = "navbar-container";
@@ -22,7 +21,7 @@ navbarContainer.innerHTML = `
     <a href="/contacts" class="navbar-menu-link">Contacts</a>
 </li>
 <li class="navbar__menu-item">
-    <a href="/staff" class="navbar-menu-link">Staff</a>
+    <a href="/stuff" class="navbar-menu-link">Stuff</a>
 </li>
 
 <div class="btn-group-list" style="display: none">
@@ -48,25 +47,25 @@ navbarContainer.innerHTML = `
 `;
 $("#navbar").append(navbarContainer);
 
-const footer = document.createElement("footer")
+const footer = document.createElement("footer");
 footer.innerHTML = `
     <div class="footer">
         <div class="footer-content">
             Copyright 2022 &copy; All Rights Reserved
         </div>
     </div>
-`
-document.body.append(footer)
+`;
+document.body.append(footer);
 
 if (window.screen.width > 805) {
-    $(".navbar-menu-list").classList.remove("navbar-active")
-    $(".navbar-menu-list").classList.add("list")
-    $(".btn-group").classList.add("desktop-menu")
+  $(".navbar-menu-list").classList.remove("navbar-active");
+  $(".navbar-menu-list").classList.add("list");
+  $(".btn-group").classList.add("desktop-menu");
 }
 
 if ($("#toggleMenu")) {
-    $("#toggleMenu").addEventListener("click", function () {
-        // $(".navbar-menu-list").classList.toggle("list")
-        $(".navbar-menu-list").classList.toggle("navbar-active")
-    })
+  $("#toggleMenu").addEventListener("click", function () {
+    // $(".navbar-menu-list").classList.toggle("list")
+    $(".navbar-menu-list").classList.toggle("navbar-active");
+  });
 }
