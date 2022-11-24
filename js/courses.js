fa8cfdb85d087f1dc3ceac3c1a118a394f52b535
00013622 <efirdavs03@gmail.com>
2022-11-24 07:29:17 -0800

    courses page content update

--- js/courses.js
+++ js/courses.js
@@ -1,61 +1,67 @@
-import { $ } from "./helper.js"
+import { $ } from "./helper.js";
 const englishCoursesList = [
-    {
-        title: "Pre-Intermediate English",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/Pre-Intermediate.png",
-    },
-    {
-        title: "Intermediate English",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/Intermediate.png",
-    },
-    {
-        title: "Advanced English",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/Advance.png",
-    }
-]
+  {
+    title: "Pre-Intermediate English",
+    description:
+      "90 minutes per lesson, 3 times a week </br> Course Length: 3-4 Months </br> Course Fee: 5,500,000 UZS",
+    img: "../images/Pre-Intermediate.png",
+  },
+  {
+    title: "Intermediate English",
+    description:
+      "90 minutes per lesson, 3 times a week </br> Course Length: 3-6 Months </br> Course Fee: 7,000,000 UZS",
+    img: "../images/Intermediate.png",
+  },
+  {
+    title: "Advanced English",
+    description:
+      "90 minutes per lesson, 5 times a week </br> Course Length: 3-9 Months </br> Course Fee: 15,000,000 UZS",
+    img: "../images/Advance.png",
+  },
+];
 
 const mathCoursesList = [
-    {
-        title: "Math for beginners",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/math.webp",
-    },
-    {
-        title: "Math tests",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/math.webp",
-    },
-    {
-        title: "Math for advanced",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, tenetur laboriosam quos fugiat qui deserunt ullam. In, magni cumque.",
-        img: "../images/math.webp",
-    }
-]
+  {
+    title: "Math for beginners",
+    description:
+      "90 minutes per lesson, 3 times a week </br> Course Length: 3-9 Months </br> Course Fee: 5,500,000 UZS",
+    img: "../images/math.webp",
+  },
+  {
+    title: "Math tests",
+    description:
+      "90 minutes per lesson, 3 times a week </br> Course Length: 3-6 Months </br> Course Fee: 7,000,000 UZS",
+    img: "../images/math.webp",
+  },
+  {
+    title: "Math for advanced",
+    description:
+      "90 minutes per lesson, 5 times a week </br> Course Length: 3-9 Months </br> Course Fee: 15,000,000 UZS",
+    img: "../images/math.webp",
+  },
+];
 
 for (let i = 0; i < 3; i++) {
-    const card = document.createElement("div");
-    card.className = "project-card";
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.className = "project-card";
+  card.innerHTML = `
     <img src="${englishCoursesList[i].img}" alt="project image"/>
 <h1>${englishCoursesList[i].title}</h1>
 <p>
     ${englishCoursesList[i].description}
 </p>
 `;
-    $("#project-en").appendChild(card);
+  $("#project-en").appendChild(card);
 }
 for (let i = 0; i < 3; i++) {
-    const card = document.createElement("div");
-    card.className = "project-card";
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.className = "project-card";
+  card.innerHTML = `
     <img src="${mathCoursesList[i].img}" alt="project image"/>
 <h1>${mathCoursesList[i].title}</h1>
 <p>
     ${mathCoursesList[i].description}
 </p>
 `;
-    $("#project-math").appendChild(card);
+  $("#project-math").appendChild(card);
 }
